8f2f50e5e6364e9c38e4e7e481414dfe472ad0fa
Jeonggyu Choi <whatasame@gmail.com>
2025-08-25 17:28:52 +0900

    Add props example

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import MyButton from "./components/MyButton";
+import { useState } from "react";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
     <div>
       <h1>Counters that update separately</h1>
-      <MyButton />
-      <MyButton />
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
     </div>
   );
+}
+
+function MyButton({ count, onClick }) {
+  return <button onClick={onClick}>Clicked {count} times</button>;
 }
