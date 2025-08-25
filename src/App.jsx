4752d749b3c9abed14ea40d437454f85fb640b08
Jeonggyu Choi <whatasame@gmail.com>
2025-08-25 16:45:08 +0900

    Add state example

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import MyButton from "./components/MyButton";
 export default function App() {
   return (
     <div>
-      <h1>Welcome to my app</h1>
+      <h1>Counters that update separately</h1>
+      <MyButton />
       <MyButton />
     </div>
   );
