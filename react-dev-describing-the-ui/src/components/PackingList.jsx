af0a9b73d37fe83be50428c1d2fbe3df7feed275
Jeonggyu Choi <jeonggyu.choi@navercorp.com>
2025-09-11 20:46:54 +0900

    Describing the UI - Conditional Rendering (Challenge 2)

--- react-dev-describing-the-ui/src/components/PackingList.jsx
+++ react-dev-describing-the-ui/src/components/PackingList.jsx
@@ -1,20 +1,22 @@
+function Item({ name, importance }) {
+  return (
+    <li className="item">
+      {name}
+      {importance > 0 && ' '}
+      {importance > 0 && <i>{`(Importance: ${importance})`}</i>}
+    </li>
+  );
+}
+
 export default function PackingList() {
   return (
     <section>
       <h1>Sally Ride's Packing List</h1>
       <ul>
-        <Item isPacked={true} name="Space suit"></Item>
-        <Item isPacked={true} name="Helmet with a golden leaf"></Item>
-        <Item isPacked={false} name="Photo of Tam"></Item>
+        <Item importance={9} name="Space suit" />
+        <Item importance={0} name="Helmet with a golden leaf" />
+        <Item importance={6} name="Photo of Tam" />
       </ul>
     </section>
-  );
-}
-
-function Item({ name, isPacked }) {
-  return (
-    <li className="item">
-      {name} {isPacked ? '✅' : '❌'}
-    </li>
   );
 }
