e9ef4bffa52c5a0407325a2538886b118e596e07
Jeonggyu Choi <whatasame@gmail.com>
2025-09-16 17:03:15 +0900

    Describing the UI - Responding to Events (Challenge 2)

--- react-dev-describing-the-ui/src/App.jsx
+++ react-dev-describing-the-ui/src/App.jsx
@@ -1,9 +1,31 @@
-import { LightSwitch } from './components/LightSwitch.jsx';
+import { useState } from 'react';
+import { ColorSwitch } from './components/ColorSwitch.jsx';
 
 export default function App() {
+  const [clicks, setClicks] = useState(0);
+
+  function handleClickOutside() {
+    setClicks((c) => c + 1);
+  }
+
+  function getRandomLightColor() {
+    let r = 150 + Math.round(100 * Math.random());
+    let g = 150 + Math.round(100 * Math.random());
+    let b = 150 + Math.round(100 * Math.random());
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+
+  function handleChangeColor() {
+    let bodyStyle = document.body.style;
+    bodyStyle.backgroundColor = getRandomLightColor();
+  }
+
   return (
-    <>
-      <LightSwitch />
-    </>
+    <div style={{ width: '100%', height: '100%' }} onClick={handleClickOutside}>
+      <ColorSwitch onChangeColor={handleChangeColor} />
+      <br />
+      <br />
+      <h2>Clicks on the page: {clicks}</h2>
+    </div>
   );
 }
---
+++ react-dev-describing-the-ui/src/components/ColorSwitch.jsx
@@ -0,0 +1,12 @@
+export function ColorSwitch({ onChangeColor }) {
+  return (
+    <button
+      onClick={(e) => {
+        e.stopPropagation();
+        onChangeColor();
+      }}
+    >
+      Change color
+    </button>
+  );
+}
