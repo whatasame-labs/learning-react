8ac52496ad9b50d7162f5b71c8aca2853e0b14b7
Jeonggyu Choi <jeonggyu.choi@navercorp.com>
2025-09-17 10:36:50 +0900

    Describing the UI - State: A Component's Memory (Challenge 4)

--- react-dev-describing-the-ui/src/components/FeedbackForm.jsx
+++ react-dev-describing-the-ui/src/components/FeedbackForm.jsx
@@ -1,24 +1,8 @@
-import { useState } from 'react';
-
 export default function FeedbackForm() {
-  const [isSent, setIsSent] = useState(false);
-  const [message, setMessage] = useState('');
-
-  if (isSent) {
-    return <h1>Thank you!</h1>;
+  function handleClick() {
+    const name = prompt('What is your name?');
+    alert(`Hello, ${name}!`);
   }
 
-  return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        alert(`Sending: "${message}"`);
-        setIsSent(true);
-      }}
-    >
-      <textarea placeholder="Message" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
-      <br />
-      <button type="submit">Sends</button>
-    </form>
-  );
+  return <button onClick={handleClick}>Greet</button>;
 }
